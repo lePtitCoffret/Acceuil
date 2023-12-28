4352e4ca42e6f1e565f7413b3b07c33986d57af7
lePtitCoffret <154817873+lePtitCoffret@users.noreply.github.com>
2023-12-28 14:53:35 +0100

    added sidebar beta 0.2

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-/* Open the sidenav */
-function openNav() {
-  document.getElementById("mySidenav").style.width = "100%";
-}
+///* Open the sidenav */
+//function openNav() {
+//  document.getElementById("mySidenav").style.width = "100%";
+//}
+//
+///* Close/hide the sidenav */
+//function closeNav() {
+//  document.getElementById("mySidenav").style.width = "0";
+//}
 
-/* Close/hide the sidenav */
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-}
+let sidebar = document.querySelector(".sidebar");
+  let closeBtn = document.querySelector("#btn");
+  let searchBtn = document.querySelector(".bx-search");
+  closeBtn.addEventListener("click", ()=>{
+    sidebar.classList.toggle("open");
+    menuBtnChange();//calling the function(optional)
+  });
+  searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
+    sidebar.classList.toggle("open");
+    menuBtnChange(); //calling the function(optional)
+  });
+  // following are the code to change sidebar button(optional)
+  function menuBtnChange() {
+   if(sidebar.classList.contains("open")){
+     closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+   }else {
+     closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+   }
+  }
