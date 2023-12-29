7473556cf335699c3ad5f690fc1cbaafc2a58585
lePtitCoffret <154817873+lePtitCoffret@users.noreply.github.com>
2023-12-29 22:57:16 +0100

    added varous thigs

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function date(){
+    let D = new Date() ;
+    let d = D.getDate();
+    let m = D.getMonth();
+    let y = D.getFullYear();
+    d = d.toString();
+    m = m.toString();
+    y = y.toString();
+    let ch = d + "/" + m +  "/" + y;
+    document.getElementById("date").innerHTML = ch;
+}
+
+
+
+
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
-  document.body.style.backgroundColor = "white";
 }
